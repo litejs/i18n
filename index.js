190497c7938d0e4005acbbe0ada3b85b52d84fe6
Lauri Rooden <lauri@rooden.ee>
2019-09-13 18:25:58 +0300

    Add i18n.number

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
 !function(exports) {
 	var currentLang
+	, cache = {}
+	, formatRe = /\\{|{\\|'|{((?:("|')(?:\\?.)*?\2|[^;\}])+?)(?:;((.).*?))?}/g
 	, globalTexts = {}
 	, hasOwn = globalTexts.hasOwnProperty
 	, list = i18n.list = []
+	, fnScope = {}
 
 	exports.i18n = i18n
 	i18n.add = add
@@ -76,5 +79,85 @@
 	}
 	/**/
 
+	/*** i18n.number ***/
+	i18n[ext["#"] = "number"] = number
+	fnScope.p = "0000000000000000"
+	function number(input, format) {
+		return (cache[format] || (cache[format] = Function(
+			"d,g",
+			"var s,i,n,r,o;return " + numStr(format)
+		)))(input, fnScope)
+	}
+	number.pre = {
+		s: "(o=d>1e9?(d/=1e9,'G'):d>1e6?(d/=1e6,'M'):d>1e3?(d/=1e3,'k'):''),"
+	}
+
+	function numStr(format) {
+		// totalLength
+		// format;0-value;NaN-value;roundPoint;negFormat
+		var conf = format.split(";")
+		, m2 = /(.*?)([\d# .,_·']*[\d\/]+)([os]?)(.*)/.exec(conf[0])
+		, m3 = /([.,\/])(\d+)(?![\d.,])/.exec(m2[2])
+		, decimals = m3 && m3[2].length || 0
+		, full = m3 ? m2[2].slice(0, m3.index) : m2[2]
+		, num = full.replace(/\D+/g, "")
+		, sLen = num.length
+		, step = decimals ? +(m3[1] === "/" ? 1 / m3[2] : num + "." + m3[2]) : num
+		, decSep = m3 && m3[1]
+		, fn = "d<0&&(d=-d,n=1)||d>0||d===0?(" + (number.pre[m2[3]] || "") + "s=" + (
+			// Use exponential notation to fix float rounding
+			// Math.round(1.005*100)/100 = 1 instead of 1.01
+			decimals ?
+			"(d+'e" + decimals + "')/" + (step + "e" + decimals) :
+			"d/" + num
+		) + ",i=~~(" + (
+			conf[2] == 1 ? "s===~~s?s:s+1" : "s+" + (conf[2] || .5)
+		) + ")*" + step
+
+		if (decimals) {
+			fn += ",r=i.toFixed(" + decimals + ")"
+			if (decSep == "/") {
+				fn += ".replace(/\\.\\d+/,'" + (
+					m3[2] == 5 ?
+					"⅕⅖⅗⅘'.charAt(5" :
+					"⅛¼⅜½⅝¾⅞'.charAt(8"
+				) + "*(i%1)-1))"
+			} else if (decSep != ".") {
+				fn += ".replace('.','" + decSep + "')"
+			}
+			if (sLen === 0) {
+				fn += ",i<1&&i!==0&&(r=r.slice(1))"
+			}
+		} else {
+			fn += ",r=(''+i)"
+		}
+		if (sLen > 1) {
+			if (decimals) sLen += decimals + 1
+			fn += ",r=(r.length<" + sLen + "?(g.p+r).slice(-" + sLen + "):r)"
+		}
+
+		if (num = full.match(/[ ,.'][\d#]+/g)) {
+			fn += ".replace(/\\d(?=" + (
+				"(((((".slice(-num.length) + num.join(")?").replace(/[ ,.]/g,"").replace(/[\d#]/g, "\\d") + ")+"
+			) + (decimals ? "\\" + decSep : "$") + ")/g,'$&" + num[0].charAt(0) + "')"
+		}
+
+		fn += (
+			// negative format
+			",n?'" + (conf[3] || "-#").replace("#", "'+r+'") + "':" +
+			(m2[1] ? "'" + m2[1]+ "'+r" : "r") +
+			// ordinal 1st
+			(m2[3] == "o" ? "+(n=d,o=g.o," +
+				(fnScope.o = getFn("ordinal", currentMap).split(";")).pop() + ")" : "") +
+			(m2[3] == "s" ? "+o" : "" ) +
+			(m2[4] ? "+'" + m2[4]+ "'" : "")
+		)
+
+		return fn + "):'" + (conf[1] || "") + "'"
+	}
+	/**/
+
+
 }(this)
+
 
--- test/index.js
+++ test/index.js
@@ -1,17 +1,18 @@
 
 require("..")
-.test("i18n", function(assert, mock) {
+.test("lib/i18n", function(assert, mock) {
 	var mod = require("../../lib/i18n.js")
 	, i18n = mod.i18n
 
 	i18n.def({
 		"et": "Eesti keeles",
 		"ar": "Arabic",
-		"en": "In English"
+		"en": "In English",
+		"fr-CH": "French (Switzerland)"
 	})
 
 	i18n.add("et", {
-		ordinal: ".",
+		ordinal: '"."',
 		Home: "Kodu",
 		Name: "Nimi",
 		button: {
@@ -20,6 +21,19 @@ require("..")
 		},
 		"a.Name": "Nimi A"
 	})
+	i18n.add("en", {
+		ordinal: "th;st;nd;rd;o[n%=100]||o[(n-20)%10]||o[0]",
+		plural: {
+			_: "n!=1"
+
+		}
+	})
+
+	i18n.add("fr-ch", {
+		ordinal: "ème;er;o[n==1?1:0]",
+		num: "#'###,01"
+	})
+
 
 	assert.equal(i18n.current, "et")
 	assert.equal(i18n("Name"), "Nimi")
@@ -28,19 +42,113 @@ require("..")
 	assert.equal(i18n("button.Name"), "Nupu nimi")
 	assert.equal(i18n("button.Home"), "Kodu")
 
-	// i18n-detect
+	// i18n.detect
 	mock.replace(Intl, "DateTimeFormat", null)
 	mod.navigator = {language: "et-EE"}
 	assert.equal(i18n.detect(), "et")
 	mod.navigator = {languages: ["zh-CN", "en-US", "ja-JP"]}
 	assert.equal(i18n.detect(), "en")
+	assert.equal(i18n.current, "en")
 	mod.navigator = {userLanguage: "et"}
 	assert.equal(i18n.detect(), "et")
 	mod.navigator = {userLanguage: "ru"}
 	assert.equal(i18n.detect("et-EE"), "et")
 	assert.equal(i18n.detect("eq"), "et")
 
+	// i18n.number
+	assert
+	.equal(i18n.number(null, "#.05"), "")
+	.equal(i18n.number(null, "#.05;-"), "-")
+	.equal(i18n.number(.34, "#,###.05"), ".35")
+	.equal(i18n.number(-.34, "#,###.05 ;;;(#)"), "(.35)")
+	.equal(i18n.number(-.34, "#,###.05 ;;;#-"), ".35-")
+	.equal(i18n.number(.34, "#,###.05 ;;;(#)"), ".35 ")
+	.equal(i18n.number(.36, "#,##0.05"), "0.35")
+	.equal(i18n.number(.31, "#,#00.05"), "00.30")
+	.equal(i18n.number(1.005, "0.01"), "1.01")
+	.equal(i18n.number(1.005, "#.01"), "1.01")
+	.equal(i18n.number(9, "#10"), "10")
+	.equal(i18n.number(-9, "#10"), "-10")
+	.equal(i18n.number(30000.65, "# ##0,01"), "30 000,65")
+	.equal(i18n.number(1234567890, "# ##1"), "1 234 567 890")
+
+	.equal(i18n.number(.70, "#.25"), ".75")
+	.equal(i18n.number(.10, "#/4"), "0")
+	.equal(i18n.number(.20, "#/4"), "¼")
+	.equal(i18n.number(.20, "0/4"), "0¼")
+	.equal(i18n.number(.30, "#/4"), "¼")
+	.equal(i18n.number(.40, "#/4"), "½")
+	.equal(i18n.number(.50, "#/4"), "½")
+	.equal(i18n.number(.60, "#/4"), "½")
+	.equal(i18n.number(.70, "#/4"), "¾")
+	.equal(i18n.number(.80, "#/4"), "¾")
+	.equal(i18n.number(.90, "#/4"), "1")
+	.equal(i18n.number(1.0, "#/4"), "1")
+	.equal(i18n.number(1.1, "#/4"), "1")
+	.equal(i18n.number(1.2, "#/4"), "1¼")
+	.equal(i18n.number(.70, "#/8", 1), "¾")
+
+	.equal(i18n.number(1.05, "#/5"), "1")
+	.equal(i18n.number(1.15, "#/5"), "1⅕")
+	.equal(i18n.number(1.25, "#/5"), "1⅕")
+	.equal(i18n.number(1.4,  "#/5"), "1⅖")
+	.equal(i18n.number(1.6,  "#/5"), "1⅗")
+	.equal(i18n.number(1.8,  "#/5"), "1⅘")
+
+	.equal(i18n.number(1235, "#,###,##,##2.00"), "1,236.00")
+	.equal(i18n.number(1235, "00,005.00"), "01,235.00")
+	//assert.equal(i18n.number(1235, "#.1{1000k1000M1000G}"), "1.2k")
+	.equal(i18n.number(123, "#.1s"), "123.0")
+	.equal(i18n.number(1235, "#.1s"), "1.2k")
+	.equal(i18n.number(1235000, "#.1s"), "1.2M")
+
+
+	function assertOrdinal(i) {
+		assert.equal(i18n.number(parseInt(i), "1o"), i)
+	}
+
+	assert.equal(i18n.detect("et"), "et")
+	;[ "0.", "1.", "2.", "3.", "101."].forEach(assertOrdinal)
+
+	assert.equal(i18n.detect("en"), "en")
+	;[
+		"0th", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th",
+		"10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th",
+		"20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th",
+		"90th", "91st", "92nd", "93rd", "94th", "95th", "96th", "97th", "98th", "99th",
+		"100th", "101st", "102nd", "103rd", "104th", "105th", "106th", "107th", "108th", "109th",
+		"110th", "111th", "112th", "113th", "114th"
+	].forEach(assertOrdinal)
+
+	assert.equal(i18n.detect("fr-CH"), "fr-ch")
+	;[ "1er", "2ème"].forEach(assertOrdinal)
+
 	assert.end()
 })
 
+
+/*
+// List of SI prefixes
+yotta  Y   1000⁸     10²⁴    1000000000000000000000000    septillion      quadrillion    1991
+zetta  Z   1000⁷     10²¹    1000000000000000000000       sextillion      trilliard      1991
+exa    E   1000⁶     10¹⁸    1000000000000000000          quintillion     trillion       1975
+peta   P   1000⁵     10¹⁵    1000000000000000             quadrillion     billiard       1975
+tera   T   1000⁴     10¹²    1000000000000                trillion        billion        1960
+giga   G   1000³     10⁹     1000000000                   billion         milliard       1960
+mega   M   1000²     10⁶     1000000                      million                        1873
+kilo   k   1000¹     10³     1000                         thousand                       1795
+hecto  h   100       10²     100                          hundred                        1795
+deca   da  10        10¹     10                           ten                            1795
+	   1         10⁰     1                            one                            –
+deci   d   10⁻¹      10⁻1    0.1                          tenth                          1795
+centi  c   100⁻¹     10⁻2    0.01                         hundredth                      1795
+milli  m   1000⁻¹    10⁻3    0.001                        thousandth                     1795
+micro  µ   1000⁻²    10⁻6    0.000001                     millionth                      1873
+nano   n   1000⁻³    10⁻9    0.000000001                  billionth       milliardth     1960
+pico   p   1000⁻⁴    10⁻12   0.000000000001               trillionth      billionth      1960
+femto  f   1000⁻⁵    10⁻15   0.000000000000001            quadrillionth   billiardth     1964
+atto   a   1000⁻⁶    10⁻18   0.000000000000000001         quintillionth   trillionth     1964
+zepto  z   1000⁻⁷    10⁻21   0.000000000000000000001      sextillionth    trilliardth    1991
+yocto  y   1000⁻⁸    10⁻24   0.000000000000000000000001   septillionth    quadrillionth  1991
+*/
 
