4472c574b4cb5612401ca8b3439950ba2d6a547e
Lauri Rooden <lauri@rooden.ee>
2019-11-11 12:09:41 +0200

    Do not use Intl.DateTimeFormat for language detection
    On Android Intl.DateTimeFormat().resolvedOptions().locale == "en-US"
    even when language in Android settings and Chrome settings is set to "et"

--- index.js
+++ index.js
@@ -118,15 +118,10 @@
 
 	/*** i18n.detect ***/
 	i18n.detect = function(fallback) {
-		var locale
-		, navigator = exports.navigator || exports
-		try {
-			// { locale: "et", timeZone: "Europe/Tallinn" }
-			locale = Intl.DateTimeFormat().resolvedOptions().locale
-		} catch(e) {}
+		var navigator = exports.navigator || exports
 
 		// navigator.userLanguage for IE, navigator.language for others
-		return use([locale, navigator.language, navigator.userLanguage].concat(
+		return use([navigator.language, navigator.userLanguage].concat(
 			navigator.languages, fallback, list[0]
 		).filter(get)[0])
 	}
